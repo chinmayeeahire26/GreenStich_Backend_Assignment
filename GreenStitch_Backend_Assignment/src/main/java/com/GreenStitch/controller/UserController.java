e7665fd139c55b77846eba218eb2c5e1f4717172
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-12 18:36:02 +0530

    Updated code with fix from source code analysis

--- GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/controller/UserController.java
+++ GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.GreenStitch.exceptions.UserException;
 import com.GreenStitch.model.UserData;
 import com.GreenStitch.repository.UserRepository;
@@ -20,53 +19,39 @@ import com.GreenStitch.service.UserService;
 @RestController
 public class UserController {
 
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
-	// register user with given details
-	
-	@PostMapping("/app/signup")
-	public ResponseEntity<UserData> registerUser(@Validated @RequestBody UserData user) throws UserException 
-	{
-		user.setPassword(passwordEncoder.encode(user.getPassword())); 
-         
-		UserData p = userService.registerUser(user);
-		
-		return new ResponseEntity<UserData>(p,HttpStatus.CREATED);
-	}
-	
-	// first time user login with Email and password and got JWT token 
-	
-	@GetMapping("/signIn")
-	public ResponseEntity<UserData> getLoggedInCustomerDetailsHandler(Authentication auth) throws BadCredentialsException{
-		
-		UserData customer= userRepository.findByEmail(auth.getName());
-		
-		if(customer!=null)
-		{
-			 return new ResponseEntity<>(customer, HttpStatus.ACCEPTED);
-		}
-		
-		throw new BadCredentialsException("Invalid Username or password");
-		
+    @Autowired
+    private UserService userService;
 
-	}
-	
-	// Authentication with JWT token 
-	
-	@GetMapping("/logged-in/user")
-	public ResponseEntity<String> LoginUser() throws UserException
-	{
-		UserData user =  userService.loginUser();
-		
-		String message = "Welcome to Shimbhu's Website  : " +user.getFullName();
-		
-		return new ResponseEntity<String>(message,HttpStatus.OK);
-	}
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // register user with given details
+    @PostMapping("/app/signup")
+    public ResponseEntity<UserData> registerUser(@Validated @RequestBody UserData user) throws UserException {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        UserData p = userService.registerUser(user);
+        return new ResponseEntity<UserData>(p, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/signIn")
+    public ResponseEntity<UserData> getLoggedInCustomerDetailsHandler(@RequestBody UserData loginData) throws BadCredentialsException {
+        UserData customer = userRepository.findByEmail(loginData.getEmail());
+        if (// **Fix: Added password validation**
+        customer != null && passwordEncoder.matches(loginData.getPassword(), customer.getPassword())) {
+            return new ResponseEntity<>(customer, HttpStatus.ACCEPTED);
+        }
+        throw new BadCredentialsException("Invalid Username or password");
+    }
+
+    // first time user login with Email and password and got JWT token
+    // Authentication with JWT token
+    @GetMapping("/logged-in/user")
+    public ResponseEntity<String> LoginUser() throws UserException {
+        UserData user = userService.loginUser();
+        String message = "Welcome to Shimbhu's Website  : " + user.getFullName();
+        return new ResponseEntity<String>(message, HttpStatus.OK);
+    }
 }
