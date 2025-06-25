e4114bb616611f21d28f1b9851dce4ad60691d6b
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-25 22:07:04 +0530

    updated GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/controller/UserController.java

--- GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/controller/UserController.java
+++ GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/controller/UserController.java
@@ -1,57 +1,41 @@
-package com.GreenStitch.controller;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import com.GreenStitch.exceptions.UserException;
-import com.GreenStitch.model.UserData;
-import com.GreenStitch.repository.UserRepository;
-import com.GreenStitch.service.UserService;
+public class FlipkartLoginTest {
 
-@RestController
-public class UserController {
+    public void loginToFlipkart() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.flipkart.com");
 
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    // register user with given details
-    @PostMapping("/app/signup")
-    public ResponseEntity<UserData> registerUser(@Validated @RequestBody UserData user) throws UserException {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        UserData p = userService.registerUser(user);
-        return new ResponseEntity<UserData>(p, HttpStatus.CREATED);
-    }
-
-    @GetMapping("/signIn")
-    public ResponseEntity<UserData> getLoggedInCustomerDetailsHandler(@RequestBody UserData loginData) throws BadCredentialsException {
-        UserData customer = userRepository.findByEmail(loginData.getEmail());
-        if (customer != null && passwordEncoder.matches(loginData.getPassword(), customer.getPassword())) {
-            // **Fix: Correct password validation logic**
-            return new ResponseEntity<>(customer, HttpStatus.ACCEPTED);
+        // Close the login popup if it appears
+        try {
+            WebElement closeButton = driver.findElement(By.cssSelector("button._2KpZ6l._2doB4z"));
+            closeButton.click();
+        } catch (Exception e) {
+            System.out.println("Login popup not displayed");
         }
-        throw new BadCredentialsException("Invalid Username or password");
-    }
 
-    // first time user login with Email and password and got JWT token
-    // Authentication with JWT token
-    @GetMapping("/logged-in/user")
-    public ResponseEntity<String> LoginUser() throws UserException {
-        UserData user = userService.loginUser();
-        String message = "Welcome to Shimbhu's Website  : " + user.getFullName();
-        return new ResponseEntity<String>(message, HttpStatus.OK);
+        // Click on the login button
+        WebElement loginButton = driver.findElement(By.cssSelector("a._1_3w1N"));
+        loginButton.click();
+
+        // Enter username and password
+        WebElement usernameField = driver.findElement(By.cssSelector("input._2IX_2-._2LYh3d"));
+        usernameField.sendKeys("your_username");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("input._2IX_2-._3mctLh"));
+        passwordField.sendKeys("your_password");
+
+        // Submit the login form
+        WebElement submitButton = driver.findElement(By.cssSelector("button._2KpZ6l._2HKlqd._3AWRsL"));
+        submitButton.click();
+
+        // Verify login success
+        // Add verification logic here
+
+        driver.quit();
     }
 }
