4fde89cd64b5b7e56d34b98398a8c8d13743cbca
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-25 17:02:21 +0530

    updated GreenStitch_Backend_Assignment/FlipkartLoginTest.java

---
+++ GreenStitch_Backend_Assignment/FlipkartLoginTest.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FlipkartLoginTest {
+
+    public void loginToFlipkart() {
+        // Set up the WebDriver
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            // Navigate to Flipkart
+            driver.get("https://www.flipkart.com");
+
+            // Close the initial login popup
+            WebElement closeButton = driver.findElement(By.cssSelector("button._2KpZ6l._2doB4z"));
+            closeButton.click();
+
+            // Click on the login button
+            WebElement loginButton = driver.findElement(By.cssSelector("a._1_3w1N"));
+            loginButton.click();
+
+            // Enter username
+            WebElement usernameField = driver.findElement(By.cssSelector("input._2IX_2-._2LYh3d.VJZDxU"));
+            usernameField.sendKeys("your_username");
+
+            // Enter password
+            WebElement passwordField = driver.findElement(By.cssSelector("input._2IX_2-._3mctLh.VJZDxU"));
+            passwordField.sendKeys("your_password");
+
+            // Submit the login form
+            WebElement submitButton = driver.findElement(By.cssSelector("button._2KpZ6l._2HKlqd._3AWRsL"));
+            submitButton.click();
+
+            // Add assertions here to verify successful login
+
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
+    }
+}
