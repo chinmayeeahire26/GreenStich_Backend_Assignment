648ce7b366b16fd2b0d0fe55c09c7ce81efb5132
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-26 20:17:33 +0530

    Updated code with fix from source code analysis

--- GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/service/UserService.java
+++ GreenStitch_Backend_Assignment/src/main/java/com/GreenStitch/service/UserService.java
@@ -5,6 +5,23 @@ import com.GreenStitch.model.UserData;
 
 public interface UserService {
 
-	public UserData registerUser(UserData user) throws UserException;
-	public UserData loginUser()  throws UserException;
+    public UserData registerUser(UserData user) throws UserException;
+
+    @Override
+    public UserData loginUser() throws UserException {
+        SecurityContext sc = SecurityContextHolder.getContext();
+        Authentication auth = sc.getAuthentication();
+        if (auth == null || !auth.isAuthenticated()) {
+            throw new UserException("User is not authenticated");
+        }
+        String userName = auth.getName();
+        if (userName == null || userName.isEmpty()) {
+            throw new UserException("Username is invalid");
+        }
+        UserData user = userRepo.findByEmail(userName);
+        if (user == null) {
+            throw new UserException("User not found");
+        }
+        return user;
+    }
 }
