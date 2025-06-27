699c48333b9ca40015e07f9585921d3aaf0740cd
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-06-27 12:58:37 +0530

    updated GreenStitch_Backend_Assignment/ZeptoLoginTest.java

---
+++ GreenStitch_Backend_Assignment/ZeptoLoginTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ZeptoLoginTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void loginToZepto() {
+        driver.get("https://www.zepto.com/login");
+        WebElement usernameField = driver.findElement(By.id("username"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+        usernameField.sendKeys("testuser");
+        passwordField.sendKeys("password123");
+        loginButton.click();
+
+        // Add assertions to verify successful login
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
