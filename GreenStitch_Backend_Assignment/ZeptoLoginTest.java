3714475d51a790fcb6ba18af68f9a949e3c227d2
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2025-12-10 16:46:48 +0530

    updated GreenStitch_Backend_Assignment/ZeptoLoginTest.java

---
+++ GreenStitch_Backend_Assignment/ZeptoLoginTest.java
@@ -0,0 +1,18 @@
+public class ZeptoLoginTest {
+    public void loginToZepto(String username, String password) {
+        // Navigate to the login page
+        driver.get("https://www.zepto.com/login");
+        // Enter username
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys(username);
+        // Enter password
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys(password);
+        // Click the login button
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        loginButton.click();
+        // Validate successful login
+        WebElement logoutButton = driver.findElement(By.id("logoutButton"));
+        assertTrue(logoutButton.isDisplayed(), "Login failed: Logout button not displayed.");
+    }
+}
