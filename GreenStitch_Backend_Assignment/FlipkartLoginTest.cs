c900d55505d71925285985d360b862bb9e255c63
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-02-03 15:02:40 +0530

    updated GreenStitch_Backend_Assignment/FlipkartLoginTest.cs

--- GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
+++ GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
@@ -2,26 +2,27 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 
-namespace AutomationTests {
-    public class LoginTests {
+namespace MyAutomationTests {
+    public class LoginTest {
         private IWebDriver driver;
 
         [SetUp]
         public void Setup() {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://example.com/login");
+            driver.Navigate().GoToUrl("https://example.com/login");
         }
 
         [Test]
-        public void LoginTest() {
+        public void TestLogin() {
             driver.FindElement(By.Id("username")).SendKeys("testuser");
             driver.FindElement(By.Id("password")).SendKeys("password123");
             driver.FindElement(By.Id("loginButton")).Click();
+
             Assert.IsTrue(driver.FindElement(By.Id("welcomeMessage")).Displayed);
         }
 
         [TearDown]
-        public void TearDown() {
+        public void Teardown() {
             driver.Quit();
         }
     }
