675661db74aff273662c3aa6f2a67bef2e5914a2
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-02-03 13:47:07 +0530

    updated GreenStitch_Backend_Assignment/FlipkartLoginTest.cs

--- GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
+++ GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
@@ -1,16 +1,28 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
 
-public class AjioLoginTest {
-    private IWebDriver driver;
+namespace AutomationTests {
+    public class LoginTests {
+        private IWebDriver driver;
 
-    public void LoginTest() {
-        driver = new ChromeDriver();
-        driver.Navigate().GoToUrl("https://www.ajio.com");
-        driver.FindElement(By.Id("username")).SendKeys("testuser");
-        driver.FindElement(By.Id("password")).SendKeys("password123");
-        driver.FindElement(By.Id("loginButton")).Click();
-        // Add assertions here to validate successful login
-        driver.Quit();
+        [SetUp]
+        public void Setup() {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://example.com/login");
+        }
+
+        [Test]
+        public void LoginTest() {
+            driver.FindElement(By.Id("username")).SendKeys("testuser");
+            driver.FindElement(By.Id("password")).SendKeys("password123");
+            driver.FindElement(By.Id("loginButton")).Click();
+            Assert.IsTrue(driver.FindElement(By.Id("welcomeMessage")).Displayed);
+        }
+
+        [TearDown]
+        public void TearDown() {
+            driver.Quit();
+        }
     }
 }
