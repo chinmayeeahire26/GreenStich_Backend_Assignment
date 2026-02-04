e5d1b947d5a0aca75f65fd0fccf26cb4c962ac5c
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-02-04 16:33:32 +0530

    updated GreenStitch_Backend_Assignment/FlipkartLoginTest.cs

--- GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
+++ GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
@@ -1,29 +1,24 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using NUnit.Framework;
 
-namespace MyAutomationTests {
-    public class LoginTest {
-        private IWebDriver driver;
+public class LoginTest {
+    private IWebDriver driver;
 
-        [SetUp]
-        public void Setup() {
-            driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("https://example.com/login");
-        }
+    public void LoginUser(string username, string password) {
+        driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://example.com/login");
 
-        [Test]
-        public void TestLogin() {
-            driver.FindElement(By.Id("username")).SendKeys("testuser");
-            driver.FindElement(By.Id("password")).SendKeys("password123");
-            driver.FindElement(By.Id("loginButton")).Click();
+        var usernameField = driver.FindElement(By.Id("username"));
+        var passwordField = driver.FindElement(By.Id("password"));
+        var loginButton = driver.FindElement(By.Id("loginButton"));
 
-            Assert.IsTrue(driver.FindElement(By.Id("welcomeMessage")).Displayed);
-        }
+        usernameField.SendKeys(username);
+        passwordField.SendKeys(password);
+        loginButton.Click();
 
-        [TearDown]
-        public void Teardown() {
-            driver.Quit();
-        }
+        // Add assertions here to verify successful login
+        // Example: Assert.AreEqual("Welcome", driver.FindElement(By.Id("welcomeMessage")).Text);
+
+        driver.Quit();
     }
 }
