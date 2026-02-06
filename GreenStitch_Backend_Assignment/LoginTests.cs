e79d445c1889d17a36c08af3ff2c8a2cbbf8df19
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-02-06 15:02:50 +0530

    updated GreenStitch_Backend_Assignment/LoginTests.cs

---
+++ GreenStitch_Backend_Assignment/LoginTests.cs
@@ -0,0 +1,29 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace AutomationTests {
+    public class LoginTests {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup() {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void LoginTest() {
+            driver.Navigate().GoToUrl("https://example.com/login");
+            driver.FindElement(By.Id("username")).SendKeys("testuser");
+            driver.FindElement(By.Id("password")).SendKeys("password123");
+            driver.FindElement(By.Id("loginButton")).Click();
+            Assert.IsTrue(driver.FindElement(By.Id("welcomeMessage")).Displayed);
+        }
+
+        [TearDown]
+        public void TearDown() {
+            driver.Quit();
+        }
+    }
+}
