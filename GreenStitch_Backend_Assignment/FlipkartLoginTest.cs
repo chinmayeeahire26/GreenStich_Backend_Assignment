e3f286c4b85b6d154595dcb61593e652f75edfbd
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-02-10 16:10:33 +0530

    updated GreenStitch_Backend_Assignment/FlipkartLoginTest.cs

--- GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
+++ GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
@@ -2,26 +2,36 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 
-namespace AutomationTests {
-    public class LoginTests {
+namespace AutomationTests
+{
+    public class LoginTest
+    {
         private IWebDriver driver;
 
         [SetUp]
-        public void Setup() {
+        public void Setup()
+        {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://example.com/login");
+            driver.Navigate().GoToUrl("https://example.com/login");
         }
 
         [Test]
-        public void LoginTest() {
-            driver.FindElement(By.Id("username")).SendKeys("testuser");
-            driver.FindElement(By.Id("password")).SendKeys("password123");
-            driver.FindElement(By.Id("loginButton")).Click();
-            Assert.IsTrue(driver.FindElement(By.Id("welcomeMessage")).Displayed);
+        public void TestLogin()
+        {
+            var usernameField = driver.FindElement(By.Id("username"));
+            var passwordField = driver.FindElement(By.Id("password"));
+            var loginButton = driver.FindElement(By.Id("loginButton"));
+
+            usernameField.SendKeys("testuser");
+            passwordField.SendKeys("password123");
+            loginButton.Click();
+
+            Assert.IsTrue(driver.Url.Contains("dashboard"));
         }
 
         [TearDown]
-        public void TearDown() {
+        public void Teardown()
+        {
             driver.Quit();
         }
     }
