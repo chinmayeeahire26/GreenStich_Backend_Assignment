d9f80fbc97003bd31035e0d92288ee39f549b873
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-07-20 18:04:11 +0530

    updated GreenStitch_Backend_Assignment/FlipkartLoginTest.cs

--- GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
+++ GreenStitch_Backend_Assignment/FlipkartLoginTest.cs
@@ -1,68 +1,22 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
-public class LoginAutomation {
+public class LoginTest {
 
     public void loginToApplication() {
-        // Set the path to the ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        // Set the base URI for the API
+        RestAssured.baseURI = "https://example.com/api";
 
-        WebDriver driver = new ChromeDriver();
-        try {
-            // Open the login page
-            driver.get("https://kairos-capgemini.azurewebsites.net/login");
-
-            // Click on Your Image
-            WebElement yourImage = driver.findElement(By.xpath("//body/app-root[1]/app-login[1]/div[1]/div[2]/div[1]/img[1]"));
-            yourImage.click();
-
-            // Click on login button
-            WebElement loginButton = driver.findElement(By.xpath("/html[1]/body[1]/app-root[1]/app-login[1]/div[1]/div[1]/app-header[1]/div[1]/div[1]/nav[1]/a[4]/img[1]"));
-            loginButton.click();
-
-            // Enter Username
-            WebElement usernameField = driver.findElement(By.xpath("//input[@id='username']"));
-            usernameField.sendKeys("shaik-raghiba.sulthana@capgemini-test.com");
-
-            // Enter Password
-            WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
-            passwordField.sendKeys("Test@1234");
-
-            // Click on Description of the image
-            WebElement descriptionImage = driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[2]/div[1]/mat-dialog-container[1]/div[1]/div[1]/app-login-register-modal[1]/div[1]/div[1]/div[1]/form[1]/div[1]/button[1]/span[2]/img[1]"));
-            descriptionImage.click();
-
-            // Click on Select Portfolio
-            WebElement selectPortfolio = driver.findElement(By.xpath("/html[1]/body[1]/app-root[1]/app-home[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/mat-form-field[1]/div[1]/div[2]/div[1]"));
-            selectPortfolio.click();
-
-            // Click on option
-            WebElement option = driver.findElement(By.xpath("//mat-option[@id='mat-option-4']"));
-            option.click();
-
-            // Click on combobox
-            WebElement comboBox = driver.findElement(By.xpath("/html[1]/body[1]/app-root[1]/app-home[1]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/mat-form-field[1]/div[1]/div[2]/div[1]/mat-select[1]/div[1]/div[1]/span[1]"));
-            comboBox.click();
-
-            // Click on Requirements Analysis
-            WebElement requirementsAnalysis = driver.findElement(By.xpath("//span[contains(text(), 'Requirements Analysis')]"));
-            requirementsAnalysis.click();
-
-            // Click on tab
-            WebElement tab = driver.findElement(By.xpath("//a[@id='mat-tab-link-6']"));
-            tab.click();
-
-            // Click on Logout
-            WebElement logout = driver.findElement(By.xpath("//a[contains(text(), 'Logout')]"));
-            logout.click();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Close the browser
-            driver.quit();
-        }
+        // Perform POST request to login endpoint with credentials
+        given()
+            .header("Content-Type", "application/json")
+            .body("{\"username\": \"testuser\", \"password\": \"testpass\"}")
+        .when()
+            .post("/login")
+        .then()
+            .statusCode(200)
+            .body("token", notNullValue());
     }
 }
