2df65c5d0a1c9c3f4aef020de7a66d22a568f00e
chinmayeeahire26 <chinmayee.ahire@capgemini.com>
2026-09-11 16:14:47 +0530

    updated GENAIQEP/MobileLoginTest.java

---
+++ GENAIQEP/MobileLoginTest.java
@@ -0,0 +1,6 @@
+methodName: loginToMobilePhone
+newCode: public class MobileLoginTest {
+    public void loginToMobilePhone() {
+        // TODO: Implement based on blueprint + test-script
+    }
+}
